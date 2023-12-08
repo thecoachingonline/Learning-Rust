c60b0bd14d12096f040189efab0045a0f76891f2
Y.Nanthachai <thecoachingonline@gmail.com>
2023-12-08 07:35:02 +0000

    Comparison Operations

---
+++ Primitive Data Types/ComparisonOperations/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let a = 1; /* true */
+    let b = false; /* 2 */
+    println!("a is {} and b is {}", a, b);
+    println!("a EQUAL to b is {}", a == b);
+    println!("a NOT EQUAL to b is {}", a != b);
+    println!("a GREATER than b is {}", a > b);
+    println!("a GREATER than OR EQUAL to b is {}", a >= b);
+    println!("a LESS than b is {}", a < b);
+    println!("a LESS than OR EQUAL to b is {}", a <= b);
+}
