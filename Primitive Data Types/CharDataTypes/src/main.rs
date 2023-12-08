90d2eda61accf675b5d3e8d441448fb9a2af6c3b
Y.Nanthachai <thecoachingonline@gmail.com>
2023-12-08 07:49:11 +0000

    Char data types

---
+++ Primitive Data Types/CharDataTypes/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let letter = 'a';
+    let number = '1';
+    let finger = '\u{261D}'; /* https://www.unicode.org/charts/PDF/U2600.pdf */
+    println!("{}\n{}\n{}", letter, number, finger);
+}
