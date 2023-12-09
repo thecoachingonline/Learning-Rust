21461a6bdc1299be4aeae65ccc22c9c9b505e5f3
Y.Nanthachai <thecoachingonline@gmail.com>
2023-12-09 10:37:43 +0000

    Crates

---
+++ Modules/Crates/src/main.rs
@@ -0,0 +1,10 @@
+/* https://crates.io */
+use rand::prelude::*;
+
+fn main() {
+    let number = random::<f64>();
+    println!("number is {}", number);
+
+    let number = thread_rng().gen_range(1..11);
+    println!("number is {}", number);
+}
