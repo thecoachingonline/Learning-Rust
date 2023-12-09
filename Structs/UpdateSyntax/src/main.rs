d15c5837cc981037b4353667b66870c76c10daff
Y.Nanthachai <thecoachingonline@gmail.com>
2023-12-09 23:09:07 +0000

    Update syntax

--- Structs/UpdateSyntax/src/main.rs
+++ Structs/UpdateSyntax/src/main.rs
@@ -1,3 +1,22 @@
+#[derive(Debug)]
+struct Shuttle {
+    name: String,
+    crew_size: u8,
+    propellant: f64
+}
 fn main() {
-    println!("Hello, world!");
+    let mut vehicle = Shuttle {
+        name: String::from("Endeavour"),
+        crew_size: 7,
+        propellant: 835958.0
+    };
+
+    let vehicle2 = Shuttle {
+        name: String::from("Discovery"),
+        ..vehicle
+    };
+
+    vehicle.name = String::from("Atlantis");
+    println!("vehicle is {:?}", vehicle);
+    println!("vehicle2 is {:?}", vehicle2);
 }
