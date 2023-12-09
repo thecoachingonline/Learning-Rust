00bb24fa2568b9b497edf152f162478ace7ddab8
Y.Nanthachai <thecoachingonline@gmail.com>
2023-12-09 08:41:30 +0000

    Slices Parameters

---
+++ References/SlicesParameters/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let message = String::from("Greetings from Earth!");
+    let first_word = get_first_word(&message[10..]);
+    println!("first_word is {}", first_word);
+}
+
+fn get_first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (index, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..index];
+        }
+    }
+    &s
+}
