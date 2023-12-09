6b64328c25ac0e582a3295b0d9cf7e5ff6f6128c
Y.Nanthachai <thecoachingonline@gmail.com>
2023-12-09 09:38:35 +0000

    Parse Strings

---
+++ Modules/ParseStrings/src/main.rs
@@ -0,0 +1,11 @@
+use std::io;
+
+fn main() {
+    let mut buffer = String::new();
+    println!("Enter a message:");
+    io::stdin().read_line(&mut buffer);
+    println!("buffer is {}", buffer);
+
+    let number: i32 = buffer.trim().parse().unwrap();
+    println!("number + 1 is {}", number + 1);
+}
