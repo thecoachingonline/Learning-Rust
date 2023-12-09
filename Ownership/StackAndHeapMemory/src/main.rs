e7f576bf2cc1f131ef38a63842295f0bc22fc687
Y.Nanthachai <thecoachingonline@gmail.com>
2023-12-09 03:03:59 +0000

    Stack and Heap

---
+++ Ownership/StackAndHeapMemory/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    /* let x = 13;
+    function1(); */
+
+    /* fn function1() {
+        let y = 3.14;
+        let z = 'a';
+        function2(); 
+    } */
+    /* function2 {
+        let arr =[1,2,3];
+    } */
+    println!("Hello, world!");
+}
