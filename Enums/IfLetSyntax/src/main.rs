7ff35103c6846b9ff86c882b84b77296a916270b
Y.Nanthachai <thecoachingonline@gmail.com>
2023-12-10 10:41:44 +0000

    if let syntax

--- Enums/IfLetSyntax/src/main.rs
+++ Enums/IfLetSyntax/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let number = Some(13);
+
+    // match number {
+    //    Some(13) => println!("thirteen"),
+    //    _ => ()
+    //}
+    if let Some(13) = number {
+        println!("thirteen");
+    }
 }
