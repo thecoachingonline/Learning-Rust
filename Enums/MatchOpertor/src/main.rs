131d64d8ac645a7853d16b54ef61ef96e9d44041
Y.Nanthachai <thecoachingonline@gmail.com>
2023-12-10 09:14:06 +0000

    Match Operator

--- Enums/MatchOpertor/src/main.rs
+++ Enums/MatchOpertor/src/main.rs
@@ -1,3 +1,16 @@
+#[derive(Debug)]
+enum Shape {
+    Circle(f64), // radius
+    Rectangle(f64, f64), // width, heigth
+    Triangle(f64, f64, f64) // sides a, b, c
+}
 fn main() {
-    println!("Hello, world!");
+    let my_shape = Shape::Rectangle(1.2, 3.4);
+    println!("my_shape is {:?}", my_shape);
+
+    match my_shape {
+        Shape::Circle(r) => println!("Circle with radius {}", r),
+        Shape::Rectangle(w, h) => println!("{} x {} Rectangle", w, h),
+        Shape::Triangle(a, b, c) => println!("Triangle with sides {}, {}, {}", a, b, c)
+    }
 }
