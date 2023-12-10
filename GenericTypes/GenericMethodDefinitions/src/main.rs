5d6e56d2bff9a3670239bbe1e2348f70afaf8d81
Y.Nanthachai <thecoachingonline@gmail.com>
2023-12-10 01:41:18 +0000

    Generic Method Definitions

--- GenericTypes/GenericMethodDefinitions/src/main.rs
+++ GenericTypes/GenericMethodDefinitions/src/main.rs
@@ -1,3 +1,27 @@
+#[derive(Debug)]
+struct Rectangle<T, U> {
+    width: T,
+    heigth: U
+}
+
+impl<T, U> Rectangle<T, U> {
+    fn get_width(&self) -> &T {
+        &self.width
+    }
+}
+
+impl Rectangle<u8, u8> {
+    fn get_perimeter(&self) -> u8 {
+        2 * self.width + 2 * self.heigth
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let rect = Rectangle {
+        width: 1u8,
+        heigth: 3u8
+    };
+    println!("rect is {:?}", rect);
+    println!("width is {}", rect.get_width());
+    println!("perimeter is {}", rect.get_perimeter());
 }
